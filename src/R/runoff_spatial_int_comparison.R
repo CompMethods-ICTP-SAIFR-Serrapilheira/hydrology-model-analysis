35d0946c56f9c9ef6d559aeaf0f25298004deaaa
Marandola12 <gabrielmarandola12@gmail.com>
2022-08-19 02:50:11 -0300

    WIP -styling and legend config

--- src/R/runoff_spatial_int_comparison.R
+++ src/R/runoff_spatial_int_comparison.R
@@ -21,19 +21,25 @@ library(zoo)
 runoff_df <- cbind.data.frame(ic_runoff_spc, pvm_runoff_spc)
 
 ggplot(runoff_df, aes(x = ic_runoff_spc, y = pvm_runoff_spc)) +
-  geom_point(shape = 1, color = "17,17,17", alpha = 0.3) +
-  geom_smooth(method = lm) +
-  geom_abline(slope = 1, intercept = 0) +
+  geom_point(shape = 1, color = "#282828", alpha = 0.3) +
+  geom_smooth(method = lm, show.legend = T, color = "#e29a25") +
+  geom_abline(slope = 1, intercept = 0, linetype = 2, color = '#e29a25') +
+  scale_x_log10(limits = c(1,50)) +
+  scale_y_log10(limits = c(1,50)) +
+  labs(x = "CAETE-IC runoff (mm)", y = "CAETE-PVM runoff (mm)") +
   theme_bw()
-
+  
 ic_runoff_rmean <- rollmean(ic_runoff_spc, k = 90, fill = NA)
 pvm_runoff_rmean <- rollmean(pvm_runoff_spc, k = 90, fill = NA)
 
 runoff_rmean_df <- cbind.data.frame(ic_runoff_rmean, pvm_runoff_rmean)
 
 ggplot(runoff_rmean_df, aes(x = seq(1,13880,1))) +
-  geom_line(aes(y = runoff_rmean_df$ic_runoff_rmean), color = '#111111') +
-  geom_line(aes(y = runoff_rmean_df$pvm_runoff_rmean), color = '#775566') +
+  geom_line(aes(y = ic_runoff_rmean, colour = '#CAETE-IC')) +
+  geom_line(aes(y = pvm_runoff_rmean, color = '#CAETE-PVM')) +
+  labs(x = 'Time (days)', y = 'Water runoff (mm)') +
+  scale_x_continuous(breaks = c(0, 365, 3650, 7300, 10950, 13880)) +
+  scale_color_manual(name = NULL, values = c("CAETE-IC" = "#111111", "CAETE-PVM" = "#775566")) +
   theme_bw()
 
 # getting the specific data section for comparison:
--- src/R/wsoil_spatial_int_comparison.R
+++ src/R/wsoil_spatial_int_comparison.R
@@ -23,9 +23,22 @@ library(zoo)
 wsoil_df <- cbind.data.frame(ic_wsoil_spc, pvm_wsoil_spc)
 
 ggplot(wsoil_df, aes(x = ic_wsoil_spc, y = pvm_wsoil_spc)) +
-  geom_point(shape = 1, color = "17,17,17", alpha = 0.3) +
-  geom_smooth(method = lm) +
-  geom_abline(slope = 1, intercept = 0) +
+  geom_point(shape = 1, color = "#282828", alpha = 0.3) +
+  geom_smooth(method = lm, show.legend = T, color = "#e29a25") +
+  geom_abline(slope = 1, intercept = 0, linetype = 2, color = "#e29a25") +
+  xlab("CAETE-IC soil water (mm)") + ylab("CAETE-PVM soil water (mm)") +
+  scale_x_continuous(limits = c(200, 500)) +
+  ggplot2::x
+theme_bw()
+
+
+ggplot(wsoil_df, aes(x = ic_wsoil_spc, y = pvm_wsoil_spc)) +
+  geom_point(shape = 1, color = "#282828", alpha = 0.3) +
+  geom_smooth(method = lm, show.legend = T, color = "#e29a25") +
+  geom_abline(slope = 1, intercept = 0, linetype = 2, color = "#e29a25") +
+  scale_x_continuous(limits = c(200,500), breaks = c(200, 300, 400, 500)) +
+  scale_y_continuous(limits = c(200,500), breaks = c(200, 300, 400, 500)) +
+  labs(x = 'CAETE-IC soil water (mm)', y = 'CAETE-PVM soil water (mm)') +
   theme_bw()
 
 ic_wsoil_rmean <- rollmean(ic_wsoil_spc, k = 90, fill = NA)
@@ -36,17 +49,19 @@ wsoil_rmean_df <- cbind.data.frame(ic_wsoil_rmean, pvm_wsoil_rmean)
 ggplot(wsoil_rmean_df, aes(x = seq(1,13880,1))) +
   geom_line(aes(y = wsoil_rmean_df$ic_wsoil_rmean), color = '#111111') +
   geom_line(aes(y = wsoil_rmean_df$pvm_wsoil_rmean), color = '#775566') +
-  theme_bw()
+  labs(x = 'Time (days)', y = 'Soil water (mm)') +
+  scale_x_continuous(breaks = c(0, 365, 3650, 7300, 10950, 13880))
+theme_bw()
 
 
 
 
 
 # CHECKLIST:
-  # theme with proper colors;
-  # save_fig sessions
-  # axis labels
-  # timestep in years instead of days
+# theme with proper colors;
+# save_fig sessions
+# axis labels
+# timestep in years instead of days
 
 
 
